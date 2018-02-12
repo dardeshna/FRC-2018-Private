732e05dd800c2b0c9b53f98770b7d620761035bd
Robbie Selwyn <robbie@selwynsoftware.com>
2018-02-11 17:44:25 -0800

    Added elevator and intake to other autos

--- src/com/palyrobotics/frc2018/auto/modes/CenterStartRightScaleAutoMode.java
+++ src/com/palyrobotics/frc2018/auto/modes/CenterStartRightScaleAutoMode.java
@@ -1,10 +1,14 @@
 package com.palyrobotics.frc2018.auto.modes;
 
 import com.palyrobotics.frc2018.auto.AutoModeBase;
+import com.palyrobotics.frc2018.behavior.ParallelRoutine;
 import com.palyrobotics.frc2018.behavior.Routine;
 import com.palyrobotics.frc2018.behavior.SequentialRoutine;
 import com.palyrobotics.frc2018.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2018.behavior.routines.drive.DriveSensorResetRoutine;
+import com.palyrobotics.frc2018.behavior.routines.elevator.ElevatorCustomPositioningRoutine;
+import com.palyrobotics.frc2018.behavior.routines.intake.IntakeDownRoutine;
+import com.palyrobotics.frc2018.behavior.routines.intake.IntakeOpenRoutine;
 import com.palyrobotics.frc2018.config.AutoDistances;
 import com.palyrobotics.frc2018.config.Constants;
 import com.palyrobotics.frc2018.util.trajectory.Path;
@@ -67,6 +71,16 @@ public class CenterStartRightScaleAutoMode extends AutoModeBase {
 
         routines.add(new DriveSensorResetRoutine());
         routines.add(new DrivePathRoutine(new Path(path), false));
+
+        //Drive path while moving elevator up and moving intake down
+        ArrayList<Routine> inTransitRoutines = new ArrayList<>();
+        inTransitRoutines.add(new DrivePathRoutine(new Path(path), false));
+        inTransitRoutines.add(new ElevatorCustomPositioningRoutine(Constants.kElevatorTopPositionInches, 15));
+        inTransitRoutines.add(new IntakeDownRoutine());
+        routines.add(new ParallelRoutine(inTransitRoutines));
+
+        //Open when everything is done to score
+        routines.add(new IntakeOpenRoutine());
 
         return new SequentialRoutine(routines);
     }
