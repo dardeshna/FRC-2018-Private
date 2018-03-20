d77d8f314bde4c2e0713c494a0a1b0a5a3d26226
JasonLiu2002 <realjasonliu2@gmail.com>
2018-03-20 15:02:49 -0700

    Updated feed forwards :) actually

--- src/com/palyrobotics/frc2018/config/Gains.java
+++ src/com/palyrobotics/frc2018/config/Gains.java
@@ -22,8 +22,8 @@ public class Gains {
 //	public static final double kForsetiDriveVelocitykRampRate = 0.0;
 	public static final double kForsetiDriveVelocitykP = 0.125;//6.0 / 2;
 	public static final double kForsetiDriveVelocitykI = 0.0;//0.001;
-	public static final double kForsetiDriveVelocitykD = 0.0;//12.4;//85 / 2;
-	public static final double kForsetiDriveVelocitykF = 0.199805;//0.258987;//0.010516;//2.624 / 2;
+    public static final double kForsetiDriveVelocitykD = 12.5;//12.4;//85 / 2;
+    public static final double kForsetiDriveVelocitykF = 0.152807;//0.258987;//0.010516;//2.624 / 2;
 	public static final int kForsetiDriveVelocitykIzone = 0;//800 / 2;
 	public static final double kForsetiDriveVelocitykRampRate = 0.0;
 	public static final Gains forsetiVelocity = new Gains(kForsetiDriveVelocitykP, kForsetiDriveVelocitykI, kForsetiDriveVelocitykD, kForsetiDriveVelocitykF,
